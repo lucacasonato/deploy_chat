3f146bc6191c0cb69f9d011c546b9785951f2c6c
Luca Casonato <hello@lcas.dev>
2021-06-12 20:57:55 +0200

    switch to fresh (#3)

--- deps.ts
+++ deps.ts
@@ -1,6 +1,1 @@
-export * from "https://deno.land/x/sift@0.3.1/mod.ts";
-export { generate as generateUUID } from "https://deno.land/std@0.97.0/uuid/v4.ts";
-export {
-  getCookies,
-  setCookie,
-} from "https://deno.land/std@0.97.0/http/cookie.ts";
+export * from "https://raw.githubusercontent.com/lucacasonato/fresh/ae4603c7313fb50f126d3abb31f900ddf7ac611b/runtime.ts";
