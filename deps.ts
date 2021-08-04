9f32991f0a0077994d5d7b5ba85df6925e0ea874
hashrock <hashrock@users.noreply.github.com>
2021-08-04 21:48:18 +0900

    chore(deps): update deps to use twind

--- deps.ts
+++ deps.ts
@@ -1,1 +1,3 @@
-export * from "https://raw.githubusercontent.com/lucacasonato/fresh/ae4603c7313fb50f126d3abb31f900ddf7ac611b/runtime.ts";
+export * from "https://raw.githubusercontent.com/lucacasonato/fresh/3bce2fb8d367666d191f2b9c460e2802bdcbf053/runtime.ts";
+import { setup, tw } from "https://esm.sh/twind";
+export { setup, tw };
--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { start } from "https://raw.githubusercontent.com/lucacasonato/fresh/ae4603c7313fb50f126d3abb31f900ddf7ac611b/server.ts";
+import { start } from "https://raw.githubusercontent.com/lucacasonato/fresh/3bce2fb8d367666d191f2b9c460e2802bdcbf053/server.ts";
 import routes from "./routes.gen.ts";
 
 start(routes);
