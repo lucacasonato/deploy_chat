4bf388e05e95900843097b4f177d2035816a2a86
Sylvain <sylvaincau31@gmail.com>
2021-09-18 21:01:17 +0800

    fix

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+/// <reference no-default-lib="true" />
+/// <reference lib="dom" />
+/// <reference lib="deno.ns" />
+/// <reference lib="deno.unstable" />
+
 import { start } from "https://raw.githubusercontent.com/lucacasonato/fresh/a929b1022ab541e937f94592ecce05e7f4ffdaef/server.ts";
 import routes from "./routes.gen.ts";
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+/** @jsx h */
+
 import {
   h,
   IS_BROWSER,
