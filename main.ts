4cbe92ed406065622a66e64412177018e3ab59a0
Sylvain <sylvaincau31@gmail.com>
2021-09-18 20:50:19 +0800

    fix

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { start } from "https://raw.githubusercontent.com/lucacasonato/fresh/ae4603c7313fb50f126d3abb31f900ddf7ac611b/server.ts";
+import { start } from "https://raw.githubusercontent.com/lucacasonato/fresh/a929b1022ab541e937f94592ecce05e7f4ffdaef/server.ts";
 import routes from "./routes.gen.ts";
 
 start(routes);
